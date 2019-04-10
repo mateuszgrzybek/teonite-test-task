08b57b867718a28d0782e201e03bae1b43b2ae9e
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-04-10 18:59:27 +0200

    Create a function to check the http response

---
+++ web-scraper/scrap.py
@@ -0,0 +1,14 @@
+from contextlib import closing
+from requests import get
+from bs4 import BeautifulSoup
+
+def is_good_response(response):
+    """Check if the given url appears to be an html doctype and check the
+    status code returned with the response.
+    """
+    return(response.status_code == 200,
+           response.text.find('html'))
+           
+def get_url(url):
+    """Get the page's html code."""
+    with closing(get(url, stream=True)) as response:
