6c85d1cb2fc59a5778c7a9a2d68c71a24e8145ef
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-04-10 19:04:48 +0200

    Create a function for websites code extraction

--- web-scraper/scrap.py
+++ web-scraper/scrap.py
@@ -8,7 +8,11 @@ def is_good_response(response):
     """
     return(response.status_code == 200,
            response.text.find('html'))
-           
+
 def get_url(url):
     """Get the page's html code."""
     with closing(get(url, stream=True)) as response:
+        if is_good_response(response):
+            return response.text
+        else:
+            print('Invalid data.')
