e9bb51ae51f67c74aefdc2ccd112bc68fad1617e
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2019-04-15 19:12:56 +0200

    Fixes among django files

--- api/app/views.py
+++ api/app/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
-from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
 from .models import Authors
 from .serializers import AuthorsSerializer
 
 class AuthorsView(ListAPIView):
-
+    """View for listing author_id: author_name pairs."""
     serializer_class = AuthorsSerializer
 
     def get_queryset(self):
-
         return Authors.objects.all()
 
-    def list(self, request):
+    def get(self, request):
         queryset = self.get_queryset()
         serialized_data = AuthorsSerializer(queryset, many=True).data
         authors_json = dict()
